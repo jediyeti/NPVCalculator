540ae1bbf3e9598c7d8e08f98e08625a5b0450aa
jediyeti <yeti@unteleported.com>
2016-06-14 13:54:02 +0300

    Initial Commit

---
+++ NPVСalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NPVСalculator
+//
+//  Created by Дмитрий Полишенко on 14.06.16.
+//  Copyright © 2016 Dmitry Polishenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
